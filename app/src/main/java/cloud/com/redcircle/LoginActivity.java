ff989f67040087a0b93139a8aecc57b3b04659e7
zhan <chenyunzhan08@126.com>
2016-04-19 18:20:42 +0800

    init

---
+++ app/src/main/java/cloud/com/redcircle/LoginActivity.java
@@ -0,0 +1,10 @@
+package cloud.com.redcircle;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhan on 16/4/19.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+}
