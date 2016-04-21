b7d2dec27fb70568f492cfe4a96035f8fed9999f
chenyunzhan <chenyunzhan08@126.com>
2016-04-21 22:10:07 +0800

    集成短信功能

--- app/src/main/java/cloud/com/redcircle/Application.java
+++ app/src/main/java/cloud/com/redcircle/Application.java
@@ -1,16 +1,55 @@
 package cloud.com.redcircle;
 
 
+import android.util.Log;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
 
 /**
  * Created by zhan on 16/4/21.
  */
 public class Application extends android.app.Application {
 
+    private static final String APPKEY = "11f391ad7b6fd";
+    private static final String APPSECRET = "2d111f4bb7f9cd61815ca519bb63a2fc";
+    protected static final String ACTIVITY_TAG="MyAndroid";
+
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        SMSSDK.initSDK(this,APPKEY,APPSECRET);
+        EventHandler eh=new EventHandler(){
+
+            @Override
+            public void afterEvent(int event, int result, Object data) {
+
+                if (result == SMSSDK.RESULT_COMPLETE) {
+                    //回调完成
+                    if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                        //提交验证码成功
+                        Log.i(Application.ACTIVITY_TAG, "first activity--------onResume()");
+
+                    }else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE){
+                        //获取验证码成功
+
+
+
+
+                    }else if (event ==SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES){
+                        //返回支持发送验证码的国家列表
+                    }
+                }else{
+                    ((Throwable)data).printStackTrace();
+                }
+            }
+        };
+        SMSSDK.registerEventHandler(eh); //注册短信回调
+
     }
+
+
 
 }
--- app/src/main/java/cloud/com/redcircle/LoginActivity.java
+++ app/src/main/java/cloud/com/redcircle/LoginActivity.java
@@ -3,15 +3,51 @@ package cloud.com.redcircle;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import cn.smssdk.SMSSDK;
 
 /**
  * Created by zhan on 16/4/19.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mVerificationCodeButton;
+    private EditText mVerificationCode;
+    private EditText mPhone;
+    private Button mLogin;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mVerificationCodeButton = (Button) findViewById(R.id.login_verificationCode_btn);
+        mPhone = (EditText) findViewById(R.id.login_phone_edit);
+        mLogin = (Button) findViewById(R.id.login_login_btn);
+        mVerificationCode = (EditText) findViewById(R.id.login_verificationCode_edit);
+        mVerificationCodeButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.login_verificationCode_btn:
+
+                if (mPhone.getText().length() > 0) {
+                    SMSSDK.getVerificationCode("86",mPhone.getText().toString());
+                }
+                break;
+            case R.id.login_login_btn:
+
+                if (mPhone.getText().length() > 0 && mVerificationCode.getText().length() > 0) {
+                    SMSSDK.submitVerificationCode("86",mPhone.getText().toString(),mVerificationCode.getText().toString());
+                }
+        }
+
     }
 }
