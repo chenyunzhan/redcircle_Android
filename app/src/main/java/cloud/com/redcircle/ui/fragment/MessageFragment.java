18175930872837698ba64c0c3b83532eadc57c91
chenyunzhan <chenyunzhan08@126.com>
2016-04-29 22:09:36 +0800

    添加消息列表

--- app/src/main/java/cloud/com/redcircle/ui/fragment/MessageFragment.java
+++ app/src/main/java/cloud/com/redcircle/ui/fragment/MessageFragment.java
@@ -1,6 +1,8 @@
 package cloud.com.redcircle.ui.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,6 +13,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import cloud.com.redcircle.R;
+import io.rong.imkit.fragment.ConversationListFragment;
+import io.rong.imlib.model.Conversation;
 
 /**
  * Created by zhan on 16/4/25.
@@ -24,11 +28,24 @@ public class MessageFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_message, container, false);
-//        mPagerSlidingTabStrip = (PagerSlidingTabStrip) rootView.findViewById(R.id.pager_tabstrip);
-//        mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
-//        mEmptyText = (TextView) rootView.findViewById(R.id.empty_layout);
-        return rootView;
+
+        View view = inflater.inflate(R.layout.fragment_message, container, false);
+
+        ConversationListFragment fragment = new ConversationListFragment();
+        Uri uri = Uri.parse("rong://" + getActivity().getApplicationInfo().packageName).buildUpon()
+                .appendPath("conversationlist")
+                .appendQueryParameter(Conversation.ConversationType.PRIVATE.getName(), "false") //设置私聊会话非聚合显示
+                .appendQueryParameter(Conversation.ConversationType.GROUP.getName(), "true")//设置群组会话聚合显示
+                .appendQueryParameter(Conversation.ConversationType.DISCUSSION.getName(), "false")//设置讨论组会话非聚合显示
+                .appendQueryParameter(Conversation.ConversationType.SYSTEM.getName(), "false")//设置系统会话非聚合显示
+                .build();
+        fragment.setUri(uri);
+
+
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.rong_content, fragment);
+        transaction.commit();
+        return view;
     }
 
 
